2b752f4ccd89f73138c0df4d165dd0d08c94d9fe
Ashish Kumar Singh <ashishkumar77393@gmail.com>
2026-02-21 19:59:14 +0530

    adjency_metrix

--- graph/adjencyMartrix.py
+++ graph/adjencyMartrix.py
@@ -2,8 +2,22 @@ n=5
 m=6
 edges=[[1,2],[2,4],[3,4],[1,3],[3,5],[5,4]]
 metrix=[[0 for _ in range(n+1)] for _ in range(n+1)]
-print(metrix)
+# print(metrix)
+# for e,v in edges:
+#     metrix[e][v]=1
+#     metrix[v][e]=1
+# print(metrix)
+# using list
+lst=[[] for _ in range(n+1)]
+print(lst)
 for e,v in edges:
-    metrix[e][v]=1
-    metrix[v][e]=1
-print(metrix)
+    lst[e].append(v)
+    lst[v].append(e)
+print(lst)
+mydict={}
+for i in range(1,n+1):
+    mydict[i]=[]
+for e,v in edges:
+    mydict[e].append(v)
+    mydict[v].append(e)
+print(mydict)
